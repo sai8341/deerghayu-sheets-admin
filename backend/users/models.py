7195be16aeb1a0ab97a939117fda192c5a103475
Sai <saikumar19123@gmail.com>
2025-12-06 23:08:09 +0530

    Add Django backend folder

---
+++ backend/users/models.py
@@ -0,0 +1,21 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    class Roles(models.TextChoices):
+        SUPERADMIN = 'admin', 'Super Admin'
+        DOCTOR = 'doctor', 'Doctor'
+        RECEPTION = 'reception', 'Receptionist'
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=15, unique=True, null=True, blank=True)
+    role = models.CharField(max_length=20, choices=Roles.choices, default=Roles.DOCTOR)
+    
+    # Login with Email
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.role})"
